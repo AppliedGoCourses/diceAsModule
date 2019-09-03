49a713c804c07cb5fda0476f0da1ef390cc16716
christophberger <christophberger@users.noreply.github.com>
2019-09-03 19:13:50 +0100

    Add Pretty() to diceprinter

--- diceprinter/diceprinter.go
+++ diceprinter/diceprinter.go
@@ -4,8 +4,15 @@ import (
 	"fmt"
 
 	"github.com/appliedgocourses/dice"
+	"github.com/common-nighthawk/go-figure"
 )
 
 func Roll(sides int) {
 	fmt.Printf("Rolling a %d-sided die: %d\n", sides, dice.Roll(sides))
+}
+
+func Pretty(sides int) {
+	out := fmt.Sprintf("%d-sided roll: %d", sides, dice.Roll(sides))
+	f := figure.NewFigure(out, "", true)
+	f.Print()
 }
